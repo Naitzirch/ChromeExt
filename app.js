a01e8087a2a230027e7ea8d23d31e2f48c9ff151
C.A. Martens <c.a.martens@umail.leidenuniv.nl>
2022-05-05 21:52:10 +0200

    yayayaya it works

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 // Naitzirch 2022
-//
-// Handle receiving backgrounds and storing them
-// for specific websites
+// 
+// Handle setting/receiving backgrounds and
+// storing them for specific websites
 
-// Define imgURL in larger scope than listener
-var imgURL; //chrome.runtime.getURL("presets/nyancat.gif");
+
+// Get the site data from storage
+var site = location.href;
+var siteList = {};
+var imgURL;
+chrome.storage.sync.get(['sites'], function(result) {
+    siteList = result.sites;
+
+    // Set background if site is found in siteList
+    if (site in siteList) {
+        imgURL = siteList[site].background;
+        document.body.style.backgroundImage = `url(${imgURL})`;
+    }
+});
+
 
 // listener
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-
+        
         imgURL = request.imageURL;
-        console.log(imgURL);
 
         // Apply background immediately
         document.body.style.backgroundImage = `url(${imgURL})`;
 
-
+        // Store background + settings for this site
+        siteList[site] = {background: imgURL};
+        chrome.storage.sync.set({sites: siteList}, function() {
+            console.log("siteList set");
+        });
+        
+        chrome.storage.sync.get(['sites'], function(result) {
+            siteList = result.sites;
+            console.log(siteList);
+        });
+        
         sendResponse({farewell: "goodbye"});
     }
 );
--- popup.js
+++ popup.js
@@ -20,18 +20,18 @@ apply.addEventListener('mouseenter', function(){
     apply.style.backgroundColor = "#bdbdbd";
 });
 
-//
+// Check if user clicked on Apply background.
 apply.addEventListener('click', function(){
     ApplyBG(imgURL.innerHTML);
 });
 
 function ApplyBG(background) {
-    //console.log(background);
-
+    // Check which tab is the running tab
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // Emit message with background to the app running on the active website
         chrome.tabs.sendMessage(tabs[0].id, {imageURL: background}, function(response) {
+            // Log app's response
             console.log(response.farewell);
         });
     });
-
 }

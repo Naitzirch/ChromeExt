ab7da5db19c5a492af1ead963fefa13964c91b11
C.A. Martens <c.a.martens@umail.leidenuniv.nl>
2022-05-07 01:46:52 +0200

    some updates I guess

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 // Query DOM
 var apply = document.getElementById('apply');
-    imgURL = document.getElementById('message');
+    imgURL = document.getElementById('imgURL');
 
 
 // Fancy button smh...
@@ -19,6 +19,72 @@ apply.addEventListener('mouseleave', function(){
 apply.addEventListener('mouseenter', function(){
     apply.style.backgroundColor = "#bdbdbd";
 });
+
+// Input field logic
+imgURL.addEventListener('click', function(){
+    if (imgURL.innerHTML === "img URL") {
+        imgURL.innerHTML = "";
+    }
+});
+
+// Prevent users from pasting in rich content
+var ce = document.querySelector('[contenteditable]');
+ce.addEventListener('paste', function (e) {
+    e.preventDefault();
+    var text = e.clipboardData.getData('text/plain');
+    document.execCommand('insertText', false, text);
+});
+
+// Collapsibles
+var collIcon = document.getElementsByClassName("collIcon");
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    this.firstElementChild.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+// Color tab logic
+
+var colorWell;
+var defaultColor = document.getElementById('hex-code').innerHTML;
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+    colorWell = document.querySelector("#colorWell");
+    colorWell.value = defaultColor;
+    colorWell.addEventListener("input", updateFirst, false);
+    colorWell.addEventListener("change", updateAll, false);
+    colorWell.select();
+}
+
+function updateFirst(event) {
+    var cd = document.querySelector("#color-display");
+    var hexCode = document.getElementById('hex-code');
+
+    if (cd) {
+        cd.style.backgroundColor = event.target.value;
+        hexCode.innerHTML = event.target.value;
+    }
+}
+
+function updateAll(event) {
+    document.querySelectorAll("#color-display").forEach(function(cd) {
+        //cd.style.backgroundColor = event.target.value;
+    });
+}
+  
+
 
 // Check if user clicked on Apply background.
 apply.addEventListener('click', function(){
